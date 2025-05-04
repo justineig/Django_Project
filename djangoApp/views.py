a5c140903e0d037fdb0d7035a433008ab683910c
justineig <igirajust@gmail.com>
2025-05-04 21:18:53 +0800

    Templates

--- djangoApp/views.py
+++ djangoApp/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.utils import timezone
 
-# Create your views here.
+def home(request):
+    return render(request, "Home.html")
+
+
+def advanced_news(request):
+     news_items = [
+        {
+            'title': 'New Breakthrough in Renewable Energy',
+            'category': 'Technology',
+            'pub_date': timezone.now() - timezone.timedelta(days=1),
+            'views': 1245
+        },
+        {
+            'title': 'Global Summit on Climate Change',
+            'category': 'Environment',
+            'pub_date': timezone.now() - timezone.timedelta(days=2),
+            'views': 892
+        }
+        ]
+     context = {
+        'news_items': news_items,
+    }
+     return render(request, "news_advanced.html", context)
