7df2ae7d27f91a7c9d0e8e8346ba887a0bd5e555
justineig <igirajust@gmail.com>
2025-05-07 18:04:24 +0800

    Updates

--- Bands/urls.py
+++ Bands/urls.py
@@ -2,9 +2,11 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('Musician/<int:musician_id>/', views.musician, name='musician'),
-    path('All_Muscians/', views.allmusicians, name='Musicians'),
-    path('Bands/', views.band_list, name='bands'),
-    path('Bands_details/<int:band_id>/', views.band_detail, name='band'),
-    path('Venues', views.venue_list, name='venues'),
+    path('musicians/', views.all_musicians, name='all_musicians'),
+    path('musicians/<int:musician_id>/', views.musician_detail, name='musician_detail'),
+    
+    path('bands/', views.bands, name='band_list'),
+    path('bands/<int:band_id>/', views.band, name='band_detail'),
+    
+    path('venues/', views.venue, name='venue_list'),
 ]
--- Bands/views.py
+++ Bands/views.py
@@ -1,33 +1,34 @@
 from django.shortcuts import get_object_or_404, render
-from Bands.models import Musicians,Venue,Room,Band
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator
+from .models import Musicians, Venue, Room, Band
 
-def musician(request, musician_id):
-    musician = get_object_or_404(Musicians, id=musician_id)
-    return render(request, 'Muscian.html', {'musician': musician})
+def musician_detail(request, musician_id):
+    musician = get_object_or_404(
+        Musicians,
+        pk=musician_id
+    )
+    return render(request, 'musician_details.html', {'musician': musician})
 
-def allmusicians(request):
-    musician_list = Musicians.objects.all().order_by('fullNames')
-    paginator = Paginator(musician_list, 3) 
+def all_musicians(request):
+    musician_list = Musicians.objects.all().only('id', 'fullNames', 'telephone')
+    paginator = Paginator(musician_list, 3)
     
-    page = request.GET.get('page')
-    try:
-        musicians = paginator.page(page)
-    except PageNotAnInteger:
-        musicians = paginator.page(1)
-    except EmptyPage:
-        musicians = paginator.page(paginator.num_pages)
+    page_number = request.GET.get('page', 1)
+    page_obj = paginator.get_page(page_number)
     
-    return render(request, "allmusicians.html", {'musicians': musicians, 'page': musicians})
+    return render(request, "all_musician.html", {'page_obj': page_obj})
 
-def band_list(request):
-    bands= Band.objects.all()
-    return render(request, 'bands.html', {'bands':bands})
+def bands(request):
+    bands = Band.objects.all().prefetch_related('musicians')
+    return render(request, 'band_list.html', {'bands': bands})
 
-def band_detail(request, band_id):
-    band = Band.objects.get(pk=band_id)
+def band(request, band_id):
+    band = get_object_or_404(
+        Band.objects.prefetch_related('musicians'),
+        pk=band_id
+    )
     return render(request, 'band_details.html', {'band': band})
 
-def venue_list(request):
+def venue(request):
     venues = Venue.objects.prefetch_related('room_set')
-    return render(request, 'venues.html', {'venues': venues})
+    return render(request, 'venue_list.html', {'venues': venues})
